78fc800c9913a86c4e9a99045cc72ae151636e97
David Avsajanishvili <david@davidavs.com>
2023-10-31 14:21:54 +0000

    Rename to make compatible with the original model

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openscad-nightly pencilbox.scad -D variant='"v1inner"' -o pencilbox-v1-inner.stl
-openscad-nightly pencilbox.scad -D variant='"v1outer"' -o pencilbox-v1-outer.stl
-openscad-nightly pencilbox.scad -D variant='"v2inner"' -o pencilbox-v2-inner.stl
-openscad-nightly pencilbox.scad -D variant='"v2outer"' -o pencilbox-v2-outer.stl
+openscad-nightly pencilbox.scad -D variant='"v1inner"' -o pencil-cup-v1-wood.stl
+openscad-nightly pencilbox.scad -D variant='"v1outer"' -o pencil-cup-v1-color.stl
+openscad-nightly pencilbox.scad -D variant='"v2inner"' -o pencil-cup-v2-wood.stl
+openscad-nightly pencilbox.scad -D variant='"v2outer"' -o pencil-cup-v2-color.stl
